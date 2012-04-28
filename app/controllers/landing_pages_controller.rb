ac03cac8b87a18e46a7342be0e68064790a8224b
Tristan Kromer <TK@TristanKromer.com>
2012-04-27 23:56:41 -0700

    Added quick FAQ.

--- app/controllers/landing_pages_controller.rb
+++ app/controllers/landing_pages_controller.rb
@@ -4,4 +4,8 @@ class LandingPagesController < ApplicationController
 
   end
 
+  def faq
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Leanstartupcircle::Application.routes.draw do
   root :to => redirect("https://groups.google.com/forum/?fromgroups#!forum/lean-startup-circle")
+  match "faq.html" => "landing_pages#faq"
+  match "faq" => "landing_pages#faq"
 end
