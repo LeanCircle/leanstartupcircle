5cee34f17d59563959a6523f58846f7ce02fa298
James Bond <jamesbond.home@gmail.com>
2012-08-07 13:51:46 -0700

    heroku requires initialize_on_precompile to be false

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,9 @@ Leanstartupcircle::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
+  # Using devise on Heroku with Ruby on Rails 3.1 requires:
+  config.assets.initialize_on_precompile = false
+
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
