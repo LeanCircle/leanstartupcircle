43d03517080d4996f25edc1309a1c32d70a6880e
Tristan Kromer <TK@TristanKromer.com>
2012-11-24 22:20:04 -0500

    Add a nav bar of some sort. Lots of reshuffling.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,11 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Leanstartupcircle::Application.initialize!
+
+Leanstartupcircle::Application.configure do
+  config.action_controller.session = {
+    :session_domain => 'leanstartupcircle.com',
+    :session_key => '_leanstartupcircle',
+    :secret      => AppConfig['secret_token']
+  }
+end
