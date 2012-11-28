1cc1f0f0828cf1c0f5d4a34efcb345affb6cef11
Tristan Kromer <TK@TristanKromer.com>
2012-11-28 11:27:55 -0500

    Add flash messages.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,23 @@
 //= require jquery_ujs
 //= require_tree .
 
+
+Notifications = {
+  show_any : function() {
+    $('.notice, .alert, .success, .info').delay(400).slideDown(300);
+  },
+  handle_close : function() {
+    $('.close').live('click', function() {
+        $(this).parent().slideUp(300)
+    });
+  }
+}
+
+$(document).ready(function() {
+    $(function () {
+        Notifications.show_any();
+    });
+    $(function () {
+        Notifications.handle_close();
+    });
+});
