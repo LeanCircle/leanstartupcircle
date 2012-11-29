13337a4b8b9fcf5cb93416cc7ad5c077853fda17
Tristan Kromer <TK@TristanKromer.com>
2012-11-28 20:25:53 -0500

    Add basic show page for users.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Leanstartupcircle::Application.routes.draw do
 
-  # Devise routes
-  devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks"}
-  devise_scope :user do
-    get "sign_in", :to => "devise/sessions#new", :as => :sign_in
-    delete "sign_out", :to => "devise/sessions#destroy", :as => :sign_out
-  end
-
-  # Jobs routes
+  # Jobs subdomain routes
   constraints :subdomain => "jobs" do
     match "/" => "landing_pages#jobs", :as => :jobs
     match "/users/employer_sign_up" => "users#employer_sign_up"
@@ -17,39 +10,54 @@ Leanstartupcircle::Application.routes.draw do
     match "/thanks_for_applying" => "landing_pages#thanks_for_applying"
   end
 
-  # Meetup routes
+  # Meetup subdomain routes
   constraints :subdomain => "meetups" do
     resources :meetups, :except => [:edit, :update, :destroy], :path => "/"
   end
 
-  # Static page routes
-  [ :guidelines,
-    :team,
-    :moderation_guidelines].each do |static_page|
-    match "/#{static_page}" => "static_pages##{static_page}", :as => static_page
-  end
-  match "faq.html" => "static_pages#guidelines"
-  match "faq" => "static_pages#guidelines"
+  # Main domain routes
+  constraints :subdomain => false do
 
-  # Contact message routes
-  resources :contact_messages, :only => [:new, :create]
-  match "/contact" => "contact_messages#new", :as => :contact
-  match "/thanks_for_your_message" => "contact_messages#thanks", :as => :contact_thanks
+    # User routes
+    resources :users, :only => [:show]
 
-  # Sitemaps
-  match 'sitemap.xml' => 'sitemaps#sitemap'
-
-  # Admin
-  namespace :admin do
-    match "/" => "base#dashboard", :as => :admin_dashboard
-    resources :meetups
-    resources :users do
-      get :imitate
+    # Static page routes
+    [ :guidelines,
+      :team,
+      :moderation_guidelines].each do |static_page|
+      match "/#{static_page}" => "static_pages##{static_page}", :as => static_page
     end
-    resources :authentications, :only => [:index, :destroy]
+    match "faq.html" => "static_pages#guidelines"
+    match "faq" => "static_pages#guidelines"
+
+    # Contact message routes
+    resources :contact_messages, :only => [:new, :create]
+    match "/contact" => "contact_messages#new", :as => :contact
+    match "/thanks_for_your_message" => "contact_messages#thanks", :as => :contact_thanks
+
+    # Sitemaps
+    match 'sitemap.xml' => 'sitemaps#sitemap'
+
+    # Devise routes
+    devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks"}
+    devise_scope :user do
+      get "sign_in", :to => "devise/sessions#new", :as => :sign_in
+      delete "sign_out", :to => "devise/sessions#destroy", :as => :sign_out
+    end
+
+    # Admin
+    namespace :admin do
+      match "/" => "base#dashboard", :as => :admin_dashboard
+      resources :meetups
+      resources :users do
+        get :imitate
+      end
+      resources :authentications, :only => [:index, :destroy]
+
+    end
+
+    root :to => "landing_pages#home"
 
   end
-
-  root :to => "landing_pages#home"
 
 end
