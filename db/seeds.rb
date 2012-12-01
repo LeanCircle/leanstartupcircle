59075fdf75c690b13eec2f67305c52a38f8cb5d6
Tristan Kromer <TK@TristanKromer.com>
2012-12-01 08:26:36 -0800

    Add TK admin to seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+user = User.find_or_create_by_full_name_and_role(:name => "Tristan Kromer",
+                                                 :role => "admin",
+                                                 :confirmed_at => Time.now,
+                                                 :confirmation_sent_at => Time.now)
+user.authentications << Authentication.find_or_create_by_uid_and_provider(:provider => "meetup",
+                                                                          :uid => "10786373",
+                                                                          :token => "de7f8e88aa93e092f5d68701213ac02e",
+                                                                          :url => "http://www.meetup.com/members/10786373")
+user.authentications << Authentication.find_or_create_by_uid_and_provider(:provider => "linkedin",
+                                                                          :uid => "_EIeQesQRo",
+                                                                          :token => "505fedf9-c75a-4d24-9136-7b361e60e525",
+                                                                          :secret => "5138fc08-66b6-4d5d-a329-fe2c1f64dc52",
+                                                                          :image => "http://m3.licdn.com/mpr/mprx/0_yrLwHB6Ti7tGyxgoYqqqHqn0iu52pMpoOtAqHzLxpolpijIEr9zbQvllDTL1xYj6g1FvFtUuMPTL",
+                                                                          :url => "http://www.linkedin.com/in/tristankromer")
