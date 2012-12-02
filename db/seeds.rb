bd31cbc52b8801e695f62513fca5d74ca3b7f7ec
Tristan Kromer <TK@TristanKromer.com>
2012-12-02 08:37:29 -0800

    Fix seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ user = User.find_or_create_by_name_and_role(:name => "Tristan Kromer",
                                             :role => "admin",
                                             :confirmed_at => Time.now,
                                             :confirmation_sent_at => Time.now,
-                                            :zip_code => "94100")
+                                            :zip_code => "94110")
 user.authentications << Authentication.find_or_create_by_uid_and_provider(:provider => "meetup",
                                                                           :uid => "10786373",
                                                                           :token => "de7f8e88aa93e092f5d68701213ac02e",
