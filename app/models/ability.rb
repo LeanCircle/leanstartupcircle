44aae00e019766c8b3a1d1bf31e1f14882bf0804
Tristan Kromer <TK@TristanKromer.com>
2012-12-03 22:46:14 -0800

    Show add a meetup menu.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,16 +2,16 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-   user ||= User.new(:role => nil) # guest user (not logged in)
-   if user.role == 'admin'
-     can :manage, :all
-   elsif user.role == 'member'
-      can :create, Meetup
+    user ||= User.new(:role => nil) # guest user (not logged in)
+    if user.role == 'admin'
+      can :manage, :all
+    elsif user.role == 'member'
       can :update, Meetup, :organizer_id => user.authentications.find_by_provider('meetup').try(:uid)
-   else
-     can :read, Meetup
-     can :read, User
-   end
+    else
+      can :create, Meetup
+      can :read, Meetup
+      can :read, User
+    end
   end
 
 end
