271bc687375b7a0de2c3cfcfc3b527e416a3af55
Tristan Kromer <TK@TristanKromer.com>
2012-12-09 01:13:04 -0800

    Move nearest meetups to controller. Fix view a bit.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :find_location
+
   rescue_from CanCan::AccessDenied do |exception|
     flash[:error] = "Access denied."
     redirect_to root_url(:subdomain => false), :alert => exception.message
+  end
+
+  def find_location
+    @location = request.location
+    @location = Geocoder.search("San Francisco").first if Rails.env.development?
+    @nearest_meetups = Meetup.near(@location.coordinates, 50)
   end
 
 end
