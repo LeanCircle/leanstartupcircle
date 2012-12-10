4dd499448a5d8018e1f1db1ec820f007c7e68a87
Tristan Kromer <TK@TristanKromer.com>
2012-12-09 23:42:49 -0800

    Get menu active on hover.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -33,4 +33,14 @@ $(document).ready(function() {
     $(function () {
         Notifications.handle_close();
     });
+
+    // Will only work if string in href matches with location
+    var url = window.location;
+    $('ul.nav a[href="'+ url +'"]').parents('.dropdown').addClass('active');
+
+    // Will also work for relative and absolute hrefs
+    $('ul.nav a').filter(function() {
+        return this.href == url;
+    }).parents('.dropdown').addClass('active');
 });
+
