3f562e766f497d68288a4a89aa3ba8e8832f62f1
Tristan Kromer <TK@TristanKromer.com>
2012-12-31 18:25:33 -0800

    Some spec refactoring.

--- spec/models/authentication_spec.rb
+++ spec/models/authentication_spec.rb
@@ -6,10 +6,11 @@ describe Authentication do
 
     describe "have_many groups" do
       it { should have_many :groups }
+
       describe "scoped to meetup" do
         it { @group = create :group, :organizer_id => "12345"
-           @authentication = create :authentication, :provider => "meetup", :uid => "12345"
-           @authentication.groups.should include(@group) }
+             @authentication = create :authentication, :provider => "meetup", :uid => "12345"
+             @authentication.groups.should include(@group) }
         # TODO: Uncomment this when have an idea how to actually do it in the model.
         #it { @group = create :group, :organizer_id => "12345"
         #     @authentication = create :authentication, :provider => "twitter", :uid => "12345"
@@ -26,13 +27,14 @@ describe Authentication do
     describe "uniqueness of uid" do
       it { should validate_uniqueness_of :uid }
       it { should validate_uniqueness_of(:uid).scoped_to(:provider) }
+
       describe "scoped to provider" do
-        it { @authentication1 = create :authentication, :provider => "twitter", :uid => "12345"
-             @authentication2 = build :authentication, :provider => "twitter", :uid => "12345"
-             @authentication2.should_not be_valid }
-        it { @authentication1 = create :authentication, :provider => "twitter", :uid => "12345"
-             @authentication2 = build :authentication, :provider => "meetup", :uid => "12345"
-             @authentication2.should be_valid }
+        subject { build :authentication, :provider => "twitter", :uid => "12345" }
+
+        it { create :authentication, :provider => "twitter", :uid => "12345"
+             should_not be_valid }
+        it { create :authentication, :provider => "meetup", :uid => "12345"
+             should be_valid }
       end
     end
   end
@@ -64,44 +66,43 @@ describe Authentication do
   end
 
   describe "has method" do
-    describe "self.create_with_omniauth!:" do
-      before(:each) do
-        @twitter_hash = OmniAuth::AuthHash.new(build :omniauth_hash)
-        @user = create(:user)
-      end
+    describe "self.create_with_omniauth!" do
+      let(:user) { create :user }
+      let(:twitter_hash) { OmniAuth::AuthHash.new(build :omniauth_hash) }
 
       describe "with bad input" do
         it { expect { Authentication.create_with_omniauth!(nil, nil) }.to raise_error(ArgumentError) }
         it { expect { Authentication.create_with_omniauth!("Hello", nil) }.to raise_error(ArgumentError) }
-        it { expect { Authentication.create_with_omniauth!(nil, @user) }.to raise_error(ArgumentError) }
-        it { expect { Authentication.create_with_omniauth!(@twitter_hash, nil) }.to raise_error(ArgumentError) }
+        it { expect { Authentication.create_with_omniauth!(nil, user) }.to raise_error(ArgumentError) }
+        it { expect { Authentication.create_with_omniauth!(twitter_hash, nil) }.to raise_error(ArgumentError) }
       end
 
       describe "with valid twitter hash" do
-        it { Authentication.create_with_omniauth!(@twitter_hash, @user).should be_valid }
-        it { Authentication.create_with_omniauth!(@twitter_hash, @user).user_id.should == @user.id }
-        it { Authentication.create_with_omniauth!(@twitter_hash, @user).name.should == "Fred Flintstone" }
-        it { Authentication.create_with_omniauth!(@twitter_hash, @user).uid.should == "12345" }
-        it { Authentication.create_with_omniauth!(@twitter_hash, @user).provider.should == "twitter" }
-        it { Authentication.create_with_omniauth!(@twitter_hash, @user).token.should == "token" }
-        it { Authentication.create_with_omniauth!(@twitter_hash, @user).secret.should == "secret" }
-        it { Authentication.create_with_omniauth!(@twitter_hash, @user).image.should == "http://image.com/image.jpg" }
-        it { Authentication.create_with_omniauth!(@twitter_hash, @user).url.should == "http://homeurl.com/username" }
-        it { Authentication.create_with_omniauth!(@twitter_hash, @user).description.should == "This is who I am." }
-        it { Authentication.create_with_omniauth!(@twitter_hash, @user).location.should == "New York, NY" }
+        it { Authentication.create_with_omniauth!(twitter_hash, user).should be_valid }
+        it { Authentication.create_with_omniauth!(twitter_hash, user).user_id.should == user.id }
+        it { Authentication.create_with_omniauth!(twitter_hash, user).name.should == twitter_hash.info.name }
+        it { Authentication.create_with_omniauth!(twitter_hash, user).uid.should == twitter_hash.uid }
+        it { Authentication.create_with_omniauth!(twitter_hash, user).provider.should == twitter_hash.provider }
+        it { Authentication.create_with_omniauth!(twitter_hash, user).token.should == twitter_hash.credentials.token }
+        it { Authentication.create_with_omniauth!(twitter_hash, user).secret.should == twitter_hash.credentials.secret }
+        it { Authentication.create_with_omniauth!(twitter_hash, user).image.should == twitter_hash.info.image }
+        it { Authentication.create_with_omniauth!(twitter_hash, user).url.should == twitter_hash.info.urls.public_profile }
+        it { Authentication.create_with_omniauth!(twitter_hash, user).description.should == twitter_hash.info.description }
+        it { Authentication.create_with_omniauth!(twitter_hash, user).location.should == twitter_hash.info.location }
       end
 
       describe "with valid meetup hash" do
+        let(:meetup_hash) { OmniAuth::AuthHash.new(build :omniauth_hash, :provider => 'meetup') }
         before(:each) do
-          @meetup_hash = OmniAuth::AuthHash.new(build :omniauth_hash, :provider => 'meetup')
           Group.stub(:fetch_meetups_with_authentication) do
             @group = create :group, :name => "Meetup Group"
           end
         end
-        it { Authentication.create_with_omniauth!(@meetup_hash, @user).provider.should == "meetup" }
-        it { Authentication.create_with_omniauth!(@meetup_hash, @user)
+
+        it { Authentication.create_with_omniauth!(meetup_hash, user).provider.should == "meetup" }
+        it { Authentication.create_with_omniauth!(meetup_hash, user)
              Group.find_by_name("Meetup Group").should be_true }
-        it { Authentication.create_with_omniauth!(@twitter_hash, @user)
+        it { Authentication.create_with_omniauth!(twitter_hash, user)
              Group.find_by_name("Meetup Group").should be_false }
       end
     end
