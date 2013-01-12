c295b5892a6dc6b48275874eeaf9a52893527b41
Tristan Kromer <TK@TristanKromer.com>
2013-01-12 12:43:52 -0800

    Fix auth specs.

--- spec/models/authentication_spec.rb
+++ spec/models/authentication_spec.rb
@@ -19,23 +19,10 @@ describe Authentication do
     end
   end
 
-  describe "validates" do
+  describe "validates correctly" do
     it { should validate_presence_of :uid }
     it { should validate_presence_of :provider }
-
-    describe "uniqueness of uid" do
-      it { should validate_uniqueness_of :uid }
-      it { should validate_uniqueness_of(:uid).scoped_to(:provider) }
-
-      describe "scoped to provider" do
-        subject { build :authentication, :provider => "twitter", :uid => "12345" }
-
-        it { create :authentication, :provider => "twitter", :uid => "12345"
-             should_not be_valid }
-        it { create :authentication, :provider => "meetup", :uid => "12345"
-             should be_valid }
-      end
-    end
+    it { should validate_uniqueness_of(:uid).scoped_to(:provider) }
   end
 
   describe "has attributes" do
@@ -45,12 +32,12 @@ describe Authentication do
       it { should respond_to :provider }
     end
 
-    describe "keys" do
+    describe "for making api calls" do
       it { should respond_to :token }
       it { should respond_to :secret }
     end
 
-    describe "miscellaneous" do
+    describe "for miscellaneous use in views" do
       it { should respond_to :image }
       it { should respond_to :url }
       it { should respond_to :name }
@@ -65,35 +52,35 @@ describe Authentication do
   end
 
   describe "has method" do
-    describe "self.create_with_omniauth!" do
+    describe "self.find_or_create_with_omniauth!" do
       let(:user) { create :user }
       let(:twitter_hash) { OmniAuth::AuthHash.new(build :twitter_hash) }
       let(:meetup_hash) { OmniAuth::AuthHash.new(build :meetup_hash) }
       let(:linkedin_hash) { OmniAuth::AuthHash.new(build :linkedin_hash) }
 
       describe "with bad input" do
-        it { expect { Authentication.create_with_omniauth!(nil, nil) }.to raise_error(ArgumentError) }
-        it { expect { Authentication.create_with_omniauth!(nil, user) }.to raise_error(ArgumentError) }
+        it { expect { Authentication.find_or_create_with_omniauth!(nil, nil) }.to raise_error(ArgumentError) }
+        it { expect { Authentication.find_or_create_with_omniauth!(nil, user) }.to raise_error(ArgumentError) }
       end
 
       describe "with valid twitter hash" do
         context "and user" do
-          it { Authentication.create_with_omniauth!(twitter_hash, user).should be_valid }
-          it { Authentication.create_with_omniauth!(twitter_hash, user).user_id.should == user.id }
-          it { Authentication.create_with_omniauth!(twitter_hash, user).name.should == twitter_hash.info.name }
-          it { Authentication.create_with_omniauth!(twitter_hash, user).uid.should == twitter_hash.uid }
-          it { Authentication.create_with_omniauth!(twitter_hash, user).provider.should == twitter_hash.provider }
-          it { Authentication.create_with_omniauth!(twitter_hash, user).token.should == twitter_hash.credentials.token }
-          it { Authentication.create_with_omniauth!(twitter_hash, user).secret.should == twitter_hash.credentials.secret }
-          it { Authentication.create_with_omniauth!(twitter_hash, user).image.should == twitter_hash.info.image }
-          it { Authentication.create_with_omniauth!(twitter_hash, user).url.should == twitter_hash.info.urls.public_profile }
-          it { Authentication.create_with_omniauth!(twitter_hash, user).description.should == twitter_hash.info.description }
-          it { Authentication.create_with_omniauth!(twitter_hash, user).location.should == twitter_hash.info.location }
+          it { Authentication.find_or_create_with_omniauth!(twitter_hash, user).should be_valid }
+          it { Authentication.find_or_create_with_omniauth!(twitter_hash, user).user_id.should == user.id }
+          it { Authentication.find_or_create_with_omniauth!(twitter_hash, user).name.should == twitter_hash.info.name }
+          it { Authentication.find_or_create_with_omniauth!(twitter_hash, user).uid.should == twitter_hash.uid }
+          it { Authentication.find_or_create_with_omniauth!(twitter_hash, user).provider.should == twitter_hash.provider }
+          it { Authentication.find_or_create_with_omniauth!(twitter_hash, user).token.should == twitter_hash.credentials.token }
+          it { Authentication.find_or_create_with_omniauth!(twitter_hash, user).secret.should == twitter_hash.credentials.secret }
+          it { Authentication.find_or_create_with_omniauth!(twitter_hash, user).image.should == twitter_hash.info.image }
+          it { Authentication.find_or_create_with_omniauth!(twitter_hash, user).url.should == twitter_hash.info.urls.public_profile }
+          it { Authentication.find_or_create_with_omniauth!(twitter_hash, user).description.should == twitter_hash.info.description }
+          it { Authentication.find_or_create_with_omniauth!(twitter_hash, user).location.should == twitter_hash.info.location }
         end
 
         context "and no user" do
-          it { Authentication.create_with_omniauth!(twitter_hash).should be_valid }
-          it { Authentication.create_with_omniauth!(twitter_hash).user_id.should == nil }
+          it { Authentication.find_or_create_with_omniauth!(twitter_hash).should be_valid }
+          it { Authentication.find_or_create_with_omniauth!(twitter_hash).user_id.should == nil }
         end
       end
 
@@ -104,17 +91,17 @@ describe Authentication do
           end
         end
 
-        it { Authentication.create_with_omniauth!(meetup_hash, user).provider.should == "meetup" }
-        it { Authentication.create_with_omniauth!(meetup_hash, user)
+        it { Authentication.find_or_create_with_omniauth!(meetup_hash, user).provider.should == "meetup" }
+        it { Authentication.find_or_create_with_omniauth!(meetup_hash, user)
              Group.find_by_name("Meetup Group").should be_true }
-        it { Authentication.create_with_omniauth!(twitter_hash, user)
+        it { Authentication.find_or_create_with_omniauth!(twitter_hash, user)
              Group.find_by_name("Meetup Group").should be_false }
-        it { Authentication.create_with_omniauth!(linkedin_hash, user)
+        it { Authentication.find_or_create_with_omniauth!(linkedin_hash, user)
              Group.find_by_name("Meetup Group").should be_false }
       end
 
       describe "with valid linkedin hash" do
-        it { Authentication.create_with_omniauth!(linkedin_hash, user).provider.should == "linkedin" }
+        it { Authentication.find_or_create_with_omniauth!(linkedin_hash, user).provider.should == "linkedin" }
       end
     end
   end
