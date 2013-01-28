c74a5efac1100a365484a3992a6ef548a2fe8db3
Tristan Kromer <TK@TristanKromer.com>
2013-01-27 23:45:42 -0800

    Mod/Add some steps.

--- features/step_definitions/group_steps.rb
+++ features/step_definitions/group_steps.rb
@@ -1,6 +1,6 @@
 Given /^I have groups titled (.*?)$/ do |names|
   names.split(', ').each do |name|
-    Group.create(:name => name, :approval => true, :city => "New York")
+    FactoryGirl.create :group, :name => name, :approval => true, :city => "New York"
   end
 end
 
