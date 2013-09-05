874a321d23956c1962bc4c62046ee1d826eb0da5
Thomas Rampelberg <thomas@saunter.org>
2013-09-05 08:41:35 -0700

    Enabling newsletter frequency on the thanks page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,16 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if params[:weekly] == '1'
+      puts "weekly"
+    elsif params[:monthly] == '1'
+      puts "monthly"
+    else
+      puts "never"
+    end
+    redirect_to user_path(params[:id])
+  end
+
 end
